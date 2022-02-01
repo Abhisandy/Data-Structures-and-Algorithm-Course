7c992fd1cd0de307e790397e7b02b50cbf718940
Abhishek Saini <37812534+Abhisandy@users.noreply.github.com>
2022-02-01 15:34:20 +0530

    Add files via upload

---
+++ LinkedList/KReverse.cc
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class node{
+public:
+		int data;
+		node *next;
+
+		node(int data){
+			this->data = data;
+			next = NULL;
+		}
+};
+
+void insertAtHead(node *&head, int data){
+	if(head==NULL){
+		head = new node(data);
+		return;
+	}
+	node *n = new node(data);
+	n->next = head;
+	head = n;
+}
+
+void print(node *head){
+	while(head != NULL){
+		cout<<head->data<<"->";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+
+node * KreverseLL(node *&head, int k){
+	node * prev = NULL;
+	node * current = head;
+	node * temp;
+
+	int cnt = 1;
+	while(current!=NULL and cnt<=k){
+		//storing
+		temp = current->next;
+		//update the current
+		current->next = prev;
+		//update prev and current
+		prev = current;   
+		current =  temp;
+		cnt++;
+	}
+	if(temp!=NULL){
+		head->next = KreverseLL(temp, k);				
+	}
+	return prev;
+}
+
+int main(){
+
+	node *head = NULL;
+	insertAtHead(head, 5);
+	insertAtHead(head, 4);
+	insertAtHead(head, 3);
+	insertAtHead(head, 2);
+	insertAtHead(head, 1);
+	print(head);
+	node *nhead = KreverseLL(head, 3);
+	print(nhead);
+}
